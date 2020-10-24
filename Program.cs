52973b36f4d6778a5b9ba376d27b1b5f9ee2bff7
binkul <jacekbinkul@gmail.com>
2020-10-24 21:54:28 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sudoku_engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
